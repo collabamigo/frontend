d6e600c318ff06a85ec180e5b0d8fd0758794647
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-20 22:53:38 +0530

    test

--- src/index.js
+++ src/index.js
@@ -3,23 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as firebase from 'firebase';
-//
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAte61EJvF1myj2hs9sPbfJl-cbJVR5N2A",
-//   authDomain: "collabconnect-242d8.firebaseapp.com",
-//   projectId: "collabconnect-242d8",
-//   storageBucket: "collabconnect-242d8.appspot.com",
-//   messagingSenderId: "713675182964",
-//   appId: "1:713675182964:web:1900f01474588dc49f1221",
-//   measurementId: "G-CHLV43EN5M"
-// };
 
-// firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <h1>Heading</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
