12277e8d9cae8e5de13f30f84380a707d6e10b29
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-21 22:41:14 +0530

    temp

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-      <h1>FINALE99</h1>
+      <h1>Hope this works</h1>
   </React.StrictMode>,
   document.getElementById('root')
 );
