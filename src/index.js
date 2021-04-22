0c760b969059da7d1738283bb8acc65e5cee56ff
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-04-22 22:03:08 +0530

    shikhar dada

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
   <React.StrictMode>
-      <h1>I really hope this works</h1>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
