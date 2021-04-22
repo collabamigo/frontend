bbf12fd48c06ded7cfe0ed4dcd381f8201cdf1cc
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-22 11:52:50 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jumbotron">
+        <h1>COLLABCONNECT</h1>
+      </div>
+      <div className="row">
+          <div className="col-md-6">
+              <button type="button" className="btn btn-primary"> Signup</button>
+          </div>
+          <div className="col-md-6">
+              <button type="button" className="btn btn-primary"> Signin</button>
+          </div>
+      </div>
     </div>
   );
 }
