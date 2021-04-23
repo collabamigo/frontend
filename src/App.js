d129271c69759b056a0b520d2b80b885c82c2368
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-23 21:50:55 +0530

    Basic authentication

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import  Button from './Components/Button'
 import Collab from './Components/Collab'
 import React from 'react'
-import GSignIn from "./Components/Google_signin";
+import GoogleSignIn from "./Components/GoogleSignIn";
 
 class App extends React.Component{
     constructor(props) {
         super(props);
         this.state={
-            isActive: false
+            isActive: false,
+            user: null
         }
+    }
+
+    handleLogin = (user) => {
+        this.setState({
+            user: user
+        });
+        this.handleHide();
     }
     handleHide = () => {
         this.setState({
@@ -18,13 +26,24 @@ class App extends React.Component{
         });
       };
     render(){
+        let label ='';
+        if (this.state.user !== null)
+        {
+            label = <h1>Hello {this.state.user.getBasicProfile().getName()}</h1>
+        }
+        else
+        {
+            label = ''
+        }
   return (
     <div className="App">
-        <Collab className="jumbotron" title = "Collab Connect"/>
-        <GSignIn />
+        <Collab className="jumbotron" title = "Collab Connect" />
+        <GoogleSignIn onClick={this.handleLogin} state={!this.state.isActive} />
         <Button className ="btn btn-primary" title= "Login using IIITD" onClick={this.handleHide} state={!this.state.isActive} />
+        {label}
         <Button className="btn btn-primary" title="Ask for help" state={this.state.isActive} />
         <Button className="btn btn-primary" title="Help Others" state={this.state.isActive}  />
+
     </div>
   );}
 }
