2dae3a899aa880a72eb3d971a5f583c876f97059
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-23 11:06:19 +0530

    Basic authentication

--- src/Components/Google_signin.js
+++ src/Components/Google_signin.js
@@ -1,13 +1,13 @@
 
 function GSignIn(props){
-    function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-   alert("I am an alert box!");
-    }
+//     function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+//   console.log('Name: ' + profile.getName());
+//   console.log('Image URL: ' + profile.getImageUrl());
+//   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+//    alert("I am an alert box!");
+//     }
 
     return (<div className="g-signin2" data-onsuccess="onSignIn"></div>);
 
