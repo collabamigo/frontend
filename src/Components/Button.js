c0406606dada5afa7fb7ac86f580b25cd6cba177
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-26 23:34:27 +0530

    rework of Button.js

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,6 +1,8 @@
 
 function Button(props) {
     const {visibility, ...other} = props;
+    if (visibility===undefined)
+        console.log("WARNING: Button called without visibility attribute")
     if (visibility) {
         return (
             <button type="button" {...other}>
