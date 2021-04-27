80f48b11059b6ac22efb2d3e6564fd43d68d9b54
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-04-27 19:30:20 +0530

    Added class

--- src/Components/Help/Help.js
+++ src/Components/Help/Help.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './Help.css'
 
-const Ask = () => (
-  <div>
-    <h1 className="title is-1">Well lets help the world</h1>
-    <p>
-      lol study is joke
-    </p>
-  </div>
-);
+class Ask extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1 className="title is-1"> Dashboard </h1>
+                <p>
+                  lol study is joke
+                </p>
+            </div>
+            )
+    }
+}
 
 export default Ask;
