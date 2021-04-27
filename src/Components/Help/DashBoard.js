e1465c00dfdcf68fcc37b023cd4b4067d28540ad
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-04-28 00:28:28 +0530

    started making changes, please pull

---
+++ src/Components/Help/DashBoard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class DashBoard extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Dashboard <span className="material-icons">
+                    dashboard
+                    </span>
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default DashBoard;
