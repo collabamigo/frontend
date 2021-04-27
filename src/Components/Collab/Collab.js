372498ea1c36d8a8b10d0fc97a7575fe5d8f05a1
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-27 13:05:18 +0530

    Reindentation

--- src/Components/Collab/Collab.js
+++ src/Components/Collab/Collab.js
@@ -3,26 +3,26 @@ import './Collab.css'
 import DropdownMenu from '../DropdownMenu/DropdownMenu'
 
 class Collab extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      className : props.className,
-      title : props.title,
-      userName: props.userName,
+    constructor(props) {
+        super(props);
+        this.state = {
+            className : props.className,
+            title : props.title,
+            userName: props.userName,
+        }
     }
-  }
 
-  render() {
-    let label;
-      label = localStorage.getItem('userName')? localStorage.getItem('userName') : ""
+    render() {
+        let label;
+        label = localStorage.getItem('userName')? localStorage.getItem('userName') : ""
 
-    return(
-        <div className={this.state.className} >
-            <div className='account text-right'> {label} <DropdownMenu visibility={Boolean(localStorage.getItem('userName'))}/> </div>
-            <h1> {this.state.title} </h1>
-        </div>
-    )
-  }
+        return(
+            <div className={this.state.className} >
+                <div className='account text-right'> {label} <DropdownMenu visibility={Boolean(localStorage.getItem('userName'))}/> </div>
+                <h1> {this.state.title} </h1>
+            </div>
+        )
+    }
 }
 
 export default Collab;
