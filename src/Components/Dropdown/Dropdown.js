ea228dbc1afca30b47deee58ef05c4d46566e954
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-04-27 12:21:06 +0530

    Changed list back to div

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import Dropdown from 'react-bootstrap/Dropdown'
