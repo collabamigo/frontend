f21785baf6549b02aa49ea7d54257dc7d3bdc3b8
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-04-28 00:29:25 +0530

    work going on , please pull

--- src/Components/Help/Help.js
+++ src/Components/Help/Help.js
@@ -1,16 +1,31 @@
 import React from "react";
 import './Help.css'
+import DashBoard from './DashBoard'
 
 class Ask extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            Vendor: props.Vendor
+        }
+    }
+
     render(){
-        return(
-            <div>
-                <h1 className="title is-1"> Dashboard </h1>
-                <p>
-                  lol study is joke
-                </p>
-            </div>
+        if (this.state.Vendor){
+            return(
+                <DashBoard />
             )
+        }
+        else{
+            return(
+               <div>
+                   <h1>Sorry you can not help</h1>
+                   <p>In order to help Please register with us</p>
+
+               </div>
+            )
+        }
+
     }
 }
 
