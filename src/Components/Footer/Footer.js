55f3f4eb2a293d07ec5ed438144ecf780dba87d7
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-05-07 00:52:11 +0530

    new stuff

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css'
+
+class Footer extends React.Component {
+    render(){
+        return(
+            <div className="footer">
+                    <span className="material-icons">copyright </span>
+                     2021 Watson-Hex
+            </div>
+        )
+    }
+}
+
+export default Footer;
