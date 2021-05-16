6ad98f120cca2b6d70a82a3aeba1cdc2b9091ff8
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-05-17 01:01:52 +0530

    new files

---
+++ src/Components/Connect/Connect.js
@@ -0,0 +1,21 @@
+
+import React from 'react'
+import './Connect.css'
+
+class Connect extends React.Component{
+
+    shouldComponentUpdate () {
+        return true;
+    }
+
+    render(){
+        return(
+            <div>
+                Workin Rn ,
+                Peace out
+            </div>
+        )
+    }
+}
+
+export default Connect
