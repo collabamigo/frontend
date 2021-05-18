9bc354b5fc475876c4655b7bc91ba65e17016613
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-18 12:01:46 +0530

    minor

--- src/Components/GoogleSignIn/GoogleSignIn.js
+++ src/Components/GoogleSignIn/GoogleSignIn.js
@@ -43,7 +43,7 @@ function GoogleSignIn (props) {
     async function profileExists (googleUser) {
         if (stage==="button")
             return {
-                res:await axios.get(backend+"connect/api/profile?format=json"),
+                res:await axios.get(backend+"connect/api/profile/?format=json"),
                 googleUser: googleUser
             }
         else
