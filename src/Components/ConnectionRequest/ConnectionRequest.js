5065e3b498914919763ab1cf50ef6a08d02af59f
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-05-28 18:16:36 +0530

    ESLint

--- src/Components/ConnectionRequest/ConnectionRequest.js
+++ src/Components/ConnectionRequest/ConnectionRequest.js
@@ -3,6 +3,7 @@ import React from 'react'
 // import {Button, Form} from "react-bootstrap";
 import axios from "axios";
 import backend from "../../env";
+import {Redirect} from "react-router-dom";
 
 function useQuery() {
   return new URLSearchParams(window.location.search);
@@ -15,6 +16,7 @@ class ConnectionRequest extends React.Component{
             checked:false,
             class_change:"badge badge-warning"
         }
+        this.query = useQuery()
     }
 
     shouldComponentUpdate() {
@@ -32,9 +34,9 @@ class ConnectionRequest extends React.Component{
     }
 
     handleSubmit = (e) => {
-        const query = useQuery()
+
         axios.post(backend+"connect/approve/", {
-            request_id: query.get("request_id"),
+            request_id: this.query.get("request_id"),
             mobile: this.state.checked?1:0})
             .then(res => {
                 if (res.status === 208)
@@ -46,67 +48,75 @@ class ConnectionRequest extends React.Component{
     }
     
     render(){
-        return(   
-            <div>
-                <p>
-                    <h4>
-                        Eager to work with your next peers ?
-                        <br />
-                        Feel free to increase connectivity with your peers
-                    </h4>
+        if (this.query.get("request_id") === null)
+            return(
+                <Redirect to={
+                      {
+                          pathname: '/403',
+                      }
+                  }
+                />)
+        else
+            return(
+                <div>
+                    <p>
+                        <h4>
+                            Eager to work with your next peers ?
+                            <br />
+                            Feel free to increase connectivity with your peers
+                        </h4>
 
-                    Data we are sharing:
-                    <br />
-
-                    <span className="badge badge-success col-auto">
-                        Social Handle
-                    </span>
-
-                    <span className="badge badge-success col-auto">
-                        Github Username
-                    </span>
-
-                    <span className="badge badge-success col-auto">
-                        Email Address
-                    </span>
-
-                    <span className={this.state.class_change}>
-                        Contact Number
-                    </span>
-
-                </p>
-
-                <form>
-                    <div className="form-check">
-                        <input
-                            className="form-check-input"
-                            onChange={this.handleCheckboxChange}
-                            type="checkbox"
-                            value={this.state.checked}
-                        />
-
-                        <label
-                            className="form-check-label"
-                            htmlFor="exampleCheck1"
-                        >
-                            Check me out
-                        </label>
-
+                        Data we are sharing:
                         <br />
 
-                        <br />
+                        <span className="badge badge-success col-auto">
+                            Social Handle
+                        </span>
 
-                        <button
-                            className="btn btn-primary"
-                            onClick={this.handleSubmit.bind(this)}
-                            type="submit"
-                        >
-                            Accept request
-                        </button>
-                    </div>
-                </form>
-            </div>
-            
+                        <span className="badge badge-success col-auto">
+                            Github Username
+                        </span>
+
+                        <span className="badge badge-success col-auto">
+                            Email Address
+                        </span>
+
+                        <span className={this.state.class_change}>
+                            Contact Number
+                        </span>
+
+                    </p>
+
+                    <form>
+                        <div className="form-check">
+                            <input
+                                className="form-check-input"
+                                onChange={this.handleCheckboxChange}
+                                type="checkbox"
+                                value={this.state.checked}
+                            />
+
+                            <label
+                                className="form-check-label"
+                                htmlFor="exampleCheck1"
+                            >
+                                Check me out
+                            </label>
+
+                            <br />
+
+                            <br />
+
+                            <button
+                                className="btn btn-primary"
+                                onClick={this.handleSubmit.bind(this)}
+                                type="submit"
+                            >
+                                Accept request
+                            </button>
+                        </div>
+                    </form>
+                </div>
         )
     }
 }
