307307d8b709ca19d0bc06beb20f19be637fab06
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-06-05 17:37:06 +0530

    loading screen updated with api calling fix

--- src/Components/Help/Help.js
+++ src/Components/Help/Help.js
@@ -6,12 +6,13 @@ import axios from "axios";
 import HelpForm from "./HelpForm/HelpForm";
 
 // eslint-disable-next-line react/require-optimization
-class Ask extends React.Component {
+class Help extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            "teacher": "",
+            "isTeacher": "",
+            "loading":true,
             "skills":[],
             "name":"",
             "linkedin":"",
@@ -23,23 +24,28 @@ class Ask extends React.Component {
 
 
     componentDidMount() {
-        axios.get(backend + "connect/teacher/", {
-            params: {
-                format: "json",
-            }
-        })
-            .then((res) => {
-                this.setState({teacher: Boolean(res.data.length),
-                skills:res.data.length?res.data[0]["skills"]:[]})
-            })
+        // axios.get(backend + "connect/teacher/", {
+        //     params: {
+        //         format: "json",
+        //     }
+        // })
+        //     .then((res) => {
+        //         this.setState({teacher: Boolean(res.data.length),
+        //         skills:res.data.length?res.data[0]["skills"]:[]})
+        //     })
 
-        axios.get(backend+"connect/profile?format=json")
+        axios.get(backend+"connect/teacher?format=json")
             .then(res => {
-                this.setState({name:res.data.length ? res.data[0]["First_Name"]:[],
-                linkedin:res.data.length ? res.data[0]["Linkedin"]:[],
-                git:res.data.length ? res.data[0]["Gitname"]:[],
-                upvote:res.data.length ? res.data[0]["UpVotes"]:[],
-                downvote:res.data.length ? res.data[0]["DownVotes"]:[]});
+                if (res.data.length)
+                        this.setState({
+                            isTeacher: Boolean(res.data.length),
+                            loading: false,
+                            name:localStorage.getItem("userName"),
+                            linkedin: res.data[0]["Linkedin"],
+                            git:res.data[0]["Gitname"],
+                            upvote:res.data[0]["UpVotes"],
+                            downvote:res.data[0]["DownVotes"],
+                            skills:res.data[0]['skills']});
             })
     }
 
@@ -49,36 +55,12 @@ class Ask extends React.Component {
 
     handlerSubmit() {
         this.setState({
-            teacher: true
+            isTeacher: true
         })
     }
 
     render() {
-        if (this.state.teacher) {
-            return (
-                <DashBoard
-                    downvote={this.state.downvote}
-                    git={this.state.git}
-                    linkedin={this.state.linkedin}
-                    name={this.state.name}
-                    skills={this.state.skills}
-                    upvote={this.state.upvote}
-
-                />
-            );
-
-        }
-
-        else if (this.state.teacher === false) {
-            return (
-                <div>
-                    <HelpForm
-                        handleSubmit={this.handlerSubmit.bind(this)}
-                    />
-                </div>
-            )
-        }
-        else {
+        if (this.state.loading){
             return(
                 <div>
                     <div
@@ -91,9 +73,31 @@ class Ask extends React.Component {
                     </div>
                 </div>
                 )
+        }
 
+        else if (this.state.isTeacher) {
+            return (
+                <DashBoard
+                    downvote={this.state.downvote}
+                    git={this.state.git}
+                    linkedin={this.state.linkedin}
+                    name={this.state.name}
+                    skills={this.state.skills}
+                    upvote={this.state.upvote}
+                />
+            );
+        }
+
+        else if (!this.state.isTeacher) {
+            return (
+                <div>
+                    <HelpForm
+                        handleSubmit={this.handlerSubmit.bind(this)}
+                    />
+                </div>
+            )
         }
     }
 }
 
-export default Ask;
+export default Help;
