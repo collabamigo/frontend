8ab5669296ee52218352b0a020c62445684a1a2e
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-06-08 19:46:05 +0530

    Revert "HomePage Padding Rework"
    This reverts commit 51f9bfd4

--- src/Components/AuthenticatedHome/index.js
+++ src/Components/AuthenticatedHome/index.js
@@ -2,8 +2,8 @@
 import {Link, Route, Switch} from "react-router-dom";
 import Card from "react-bootstrap/Card";
 import React from "react";
-import {Fade} from "react-awesome-reveal";
-import {isMobile} from "react-device-detect";
+// import {Fade} from "react-awesome-reveal";
+// import {isMobile} from "react-device-detect";
 import {SvgIcon} from "../../common/SvgIcon";
 import './index.css'
 
@@ -25,143 +25,112 @@ function Index() {
             <div className="container-fluid lowl">
                 <div className="row row-cols-1 row-cols-md-3 g-4">
                     <div className="col">
-                        <Fade
-                            className=""
-                            direction={isMobile?"left":"up"}
-                            triggerOnce
-                        >
-                            <Card className="card-stype border-mine">
-                                <SvgIcon 
-                                    className="image-setting-1"
-                                    src="help_others.svg"
-                                />
+                        <Card className="card-stype border-mine">
+                            <SvgIcon 
+                                className="image-setting-1"
+                                src="help_others.svg"
+                            />
 
-                                <Card.Body className="changing-img">
+                            <Card.Body className="changing-img">
+                                <Card.Title className="card-title font-weight-bold">
+                                    HELP OTHERS
+                                </Card.Title>
 
-                                    <br />
+                                <br />
 
-                                    <Card.Text className="card-text h4 upper-text">
-                                        Solve other&apos;s doubts and be the mentor you always wanted.
-                                        Using our platform you can reach a larger curious community.
-                                    </Card.Text>
-
-
-
-                                </Card.Body>
+                                <Card.Text className="card-text masthead">
+                                    Solve other&apos;s doubts and be the mentor you always wanted.
+                                    Using our platform you can reach a larger curious community.
+                                </Card.Text>
 
                                 <Link
-                                    className="col-auto text-mine self-footer"
+                                    className="col-auto btn btn-primary"
                                     to="/help"
                                 >
-                                    <Card.Footer>
-                                        <Card.Title className="card-title font-weight-bold">
-                                            <h2 className=" text-mine">
-                                                HELP OTHERS
-                                            </h2>
-                                        </Card.Title>
-                                    </Card.Footer>
-                                </Link>   
-                            </Card>
-                        </Fade>
+                                    Help others
+                                </Link>
+
+                            </Card.Body>
+                        </Card>
                     </div>
 
                     <div className="col"> 
-                        <Fade
-                            direction={isMobile?"left":"up"}
-                            triggerOnce
-                        >
-                            <Card className="card-stype border-mine">
-                                <SvgIcon
-                                    className="image-setting-2"
-                                    src="ask_for_help.svg"
-                                />
+                        <Card className="card-stype border-mine">
+                            <SvgIcon
+                                className="image-setting-2"
+                                src="ask_for_help.svg"
+                            />
 
-                                <Card.Body className="changing-img">
-                                
+                            <Card.Body className="changing-img">
+                                <Card.Title className="card-title font-weight-bold">
+                                    ASK FOR HELP
+                                </Card.Title>
 
-                                    <Card.Text className="card-text h4 upper-text">
-                                        <br />
+                                <br />
 
-                                        <b>
-                                            Stack Overflow:404!
-                                        </b>
+                                <Card.Text className="card-text">
+                                    <b>
+                                        Stack Overflow:404!
+                                    </b>
 
-                                        <br />
-
-                                        {" "}
-                                        Answer not found,
-
-                                        The button below can solve it.
-                                    </Card.Text>
+                                    {" "}
+                                    Answer not found,
 
                                     <br />
+                                    The button below can solve it
+                                </Card.Text>
 
-                                </Card.Body>
+                                <br />
 
                                 <Link
-                                    className="col-auto text-mine self-footer"
+                                    className="col-auto btn btn-primary"
                                     to="/ask"
                                 >
-                                    <Card.Footer>
-                                        <Card.Title className="card-title font-weight-bold">
-                                            <h2 className=" text-mine">
-                                                ASK FOR HELP
-                                            </h2>
-                                        </Card.Title>
-                                    </Card.Footer>
-                                </Link>   
-                            </Card>
-                        </Fade>
+                                    Ask for help
+                                </Link>
+                            </Card.Body>
+                        </Card>
                     </div>
 
                     <div className="col">
-                        <Fade
-                            className=""
-                            direction={isMobile?"left":"up"}
-                            triggerOnce
-                        >
-                            <Card className="card-stype border-mine">
-                            
-                                <span className="h3 mt-1">
-                                    Coming Soon
-                                </span>
+                        <Card className="card-stype border-mine">
+                            <SvgIcon
+                                className="image-setting-3"
+                                src="collaborate.svg"
+                            />
 
-                                <SvgIcon
-                                    className="image-setting-3"
-                                    src="collaborate.svg"
-                                />
+                            <Card.Body className="card-body changing-img">
+                                <Card.Title className="card-title font-weight-bold">
+                                    LET&apos;S  COLLABORATE
+                                </Card.Title>
 
-                                <Card.Body className="card-body changing-img">
+                                <br />
 
+                                <Card.Text className="card-text">
+                                    Find new projects to work.
+                                    Apply for teams and Collaborations.
+                                    Lets keep the learning and helping community alive.
+                                </Card.Text>
 
-                                    <Card.Text className="card-text h4 upper-text">
-                                        Find new projects to work.
-                                        Apply for teams and Collaborations.
-                                        Lets keep the learning and helping community alive.
-                                    </Card.Text>
-
-                                    <br />
-                                </Card.Body>
-
-
-
-                                <Link
-                                    className="col-auto text-mine self-footer"
-                                    to="/"
+                                <div
+                                    className="col-auto btn btn-primary disabled"
+                                    to="/collab_connect"
                                 >
-                                    <Card.Footer>
-                                        <Card.Title className="card-title font-weight-bold disabled">
-                                            <h2 className=" text-mine">
-                                                LET&apos;S  COLLABORATE
-                                            </h2>
-                                        </Card.Title>
-                                    </Card.Footer>
-                                </Link>                            
-                            </Card>
-                        </Fade>
+                                    Coming Soon...
+                                </div>
+
+                            </Card.Body>
+                        </Card>
                     </div>
                 </div>
             </div>
+
+
+
+
+
+
+
 
 
             {/* <section className=" lowl container">
