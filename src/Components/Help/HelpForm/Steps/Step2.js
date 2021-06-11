42d07ac2a5d27bb3b26638656cb6850a4d5bc827
Heemank Verma <vheemank@gmail.com>
2021-06-11 17:20:16 +0530

    Step 2 major rework

--- src/Components/Help/HelpForm/Steps/Step2.js
+++ src/Components/Help/HelpForm/Steps/Step2.js
@@ -2,7 +2,10 @@
 import React from "react"
 import PropTypes from "prop-types";
 import Autocomplete from "../../../Ask/Autocomplete";
-import TagsInput from 'react-tagsinput'
+import TagsInput from 'react-tagsinput';
+import Card from 'react-bootstrap/Card';
+import SvgIcon from "../../../../common/SvgIcon";
+import {isMobile} from "react-device-detect";
 import './tag.css'
 
 class Step2 extends React.Component {
@@ -76,36 +79,81 @@ class Step2 extends React.Component {
             return null
         }
         return (
-            <div className="form-group justify-content-center">
-                <label className="col-auto h3">
-                    Add your skills
-                </label>
+            <section className="container-fluid lowwl mt-0">
+                <div className="row" >
+                    {isMobile?null:(
+                        <div className="col container">
+                            <div className=" float-right mr-5 mt-5">
+                                <SvgIcon
+                                    className="profile-image mt-5"
+                                    height="100%"
+                                    src="waving.svg"
+                                    width="120%"
+                                />
 
-                <div className="row justify-content-center">
-                    <div className="justify-content-center col-6">
-                        <TagsInput
-                            className="border-0"
-                            onChange={this.handleChangeTag.bind(this)}
-                            renderInput={this.renderAutocomplete.bind(this)}
-                            value={this.state.tags}
-                        />
+                                <h4 className="text-center">
+                                    Add skills you can provide help in...
+                                </h4>
+                            </div>
+                        </div>)}
+
+                    <div className="col container" >
+                        <Card className="card rounded-5 profilecard container border-primary">
+
+                            <Card.Header className="card-hf-color row">
+                                <div className="h1 float-left col-auto float-left">
+                                    Add your skill
+                                </div>
+
+                                <div className="material-icons-outlined float-right col-auto">
+                                    playlist_add
+                                </div>
+                                
+                            </Card.Header>
+
+                            <Card.Body className="pt-4 mt-0">
+                                <div>
+                                    <div className="form-group justify-content-center">
+
+                                        <div className="row justify-content-center">
+                                            <div className="justify-content-center col-mb-12">
+                                                <TagsInput
+                                                    className="border-0"
+                                                    onChange={this.handleChangeTag.bind(this)}
+                                                    renderInput={this.renderAutocomplete.bind(this)}
+                                                    value={this.state.tags}
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    
+                                </div>
+                            </Card.Body>
+
+                            <Card.Footer className="custom-modal-footer-p pb-4">
+                                <div className="row justify-content-center">
+                                    {this.previousButton()}
+
+                                    <div className="pl-2">
+
+                                        <button
+                                            className="btn btn-primary"
+                                            onChange={this.handleChange}
+                                            onClick={this.handleSubmit.bind(this)}
+                                            type="button"
+                                            value="Submit"
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            </Card.Footer>
+                        </Card>
                     </div>
                 </div>
-
-                <br />
-
-                {this.previousButton()}
-
-                <button
-                    className="btn btn-primary mb-2"
-                    onChange={this.handleChange}
-                    onClick={this.handleSubmit.bind(this)}
-                    type="button"
-                    value="Submit"
-                >
-                    Submit
-                </button>
-            </div>
+            </section>
+            
         );
     }
 }
