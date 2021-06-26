8b89d535dcf039ad37706dbd0dc4989072809c53
Heemank Verma <vheemank@gmail.com>
2021-06-26 20:44:17 +0530

    footer rework

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,30 +12,33 @@ class Footer extends React.Component {
     render() {
         return (
             <section>
-                <div className="footer justify-content-center">
+                <div className="footer row">
+                    <div className="link-footer col-md-7">
+                        <b className="float-right pr-4">
+                            
+                            {" "}
 
-                    <b className="link-footer">
-                        &#169; 2021
-                        {" "}
+                            <a
+                                className="link-footer"
+                                href="https://github.com/watson-hex"
+                                rel="noreferrer"
+                                target="_blank"
+                            >
+                                Watson-Hex &#169; 2021
+                            </a>
+                        </b>
+                    </div>
 
+                    <div className="col-md-5 float-right">
                         <a
-                            className="link-footer"
-                            href="https://github.com/watson-hex"
+                            className="text-black-50 float-right pr-3"
+                            href="https://forms.gle/MScukDTAhm2N3ixA8"
                             rel="noreferrer"
                             target="_blank"
                         >
-                            Watson-Hex
+                            Review us
                         </a>
-                    </b>
-
-                    <a
-                        className="float-right text-black-50"
-                        href="https://forms.gle/MScukDTAhm2N3ixA8"
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        Review us
-                    </a>
+                    </div>
                     
                 </div>
             </section>
