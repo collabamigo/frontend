faffd94f0af71ba81fca2232922be558efe62b0e
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-08-12 11:51:40 +0530

    Rename

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends React.Component {
                 if (error.response.status === 500)
                     alert("Unexpected error occurred. Please contact us if you see this message repeatedly.")
                 else if (error.response.status === 401)
-                    alert("Authentication error. Please try clicking/tapping on the CollabConnect logo to re-authenticate")
+                    alert("Authentication error. Please try clicking/tapping on the CollabAmigo logo to re-authenticate")
             }
             return Promise.reject(error);
           });
