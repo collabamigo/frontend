62b4d4ab979f9982339297a65a937f94cc6cf702
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-08-14 10:58:18 +0530

    Modularisation of env

--- src/env.js
+++ src/env.js
@@ -1,8 +1,10 @@
-const production=true
+const production=false
 
 let backend = ""
 
 if (production)
+    backend="https://mysterious-savannah-09391.herokuapp.com/"
+else 
     backend="https://blooming-peak-53825.herokuapp.com/"
 
 export default backend
