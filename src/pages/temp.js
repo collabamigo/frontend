666e17219d050930fc8ab5cde83d3e4b909880a0
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-09-02 17:41:45 +0530

    Basic structure

---
+++ src/pages/temp.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Layout"
+import ClubChooser from "../components/ClubChooser/Index";
+
+function temp() {
+    return (
+        <Layout>
+            <ClubChooser />
+        </Layout>)
+}
+
+export default temp
