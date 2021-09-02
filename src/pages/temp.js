157fffb73a12b965d6f59e054e7eb21fcbc2cea7
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-09-02 17:42:27 +0530

    bugfix

--- src/pages/temp.js
+++ src/pages/temp.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
-import ClubChooser from "../components/ClubChooser/Index";
+import ClubChooser from "../components/ClubChooser";
 
 function temp() {
     return (
