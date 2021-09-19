b51ab79a56324cb351ab12d2bb163bcec0e9fea1
Heemank Verma <vheemank@gmail.com>
2021-09-19 13:49:17 +0530

    proper css modulation on clubcard

--- src/components/ClubList/ClubCard.js
+++ src/components/ClubList/ClubCard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Image from 'react-bootstrap/Image';
+import {logo} from "./ClubCard.module.css";
 
 export default class ClubCard extends Component {
     static propTypes = {
