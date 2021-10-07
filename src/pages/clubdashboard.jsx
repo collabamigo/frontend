923f1c203b9a569e3bf1012c3b14909a63fe7930
Heemank Verma <heemankv@gmail.com>
2021-10-07 12:49:07 +0530

    Sidebar non css integration

--- src/pages/clubdashboard.jsx
+++ src/pages/clubdashboard.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 
 import DashboardLayout from "../components/Layout/DashboardLayout.js";
-
+import Sidebar from "../components/Sidebar/index.jsx";
 
 export default function ClubDashBoard() {
     return (
         <div>
             hello
+            <Sidebar />
+
         </div>
     )
 }
