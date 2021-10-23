b935adb257db10b54b43c44b0bc1f738b11730da
sc0rp10n-ubuntu-laptop <pragyan52yadav@gmail.com>
2021-10-23 21:50:01 +0530

    dynamic done but not showing up

--- src/components/EventAdmin/index.jsx
+++ src/components/EventAdmin/index.jsx
@@ -1,171 +1,114 @@
+/* eslint-disable react/require-default-props */
 import React from "react";
-import 'react-bootstrap';
-import Card from 'react-bootstrap/Card';
-// import { searchDivHolder } from './index.module.css';
-
+import "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import PropTypes from "prop-types";
 
 const eventList = [
     {
         timestamp: "1634640509",
         name: "Super magical Hackathon",
-        clubNames: ["Byld", "GoogleDSC"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        clubName: "Byld",
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     },
     {
         timestamp: "1634640509",
         name: "Super magical Hackathon",
-        clubNames: ["Byld", "GoogleDSC"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        clubName: "GoogleDSC",
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     },
     {
         timestamp: "1634640509",
         name: "Super magical Hackathon",
-        clubNames: ["Byld", "GoogleDSC"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+        clubName: "Byld",
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     },
     {
         timestamp: "1634640509",
         name: "Super magical Hackathon",
-        clubNames: ["GoogleDSC"],
-        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
-    }
-]
-eventList.concat(eventList)
-const props = {eventList}
+        clubName: "GoogleDSC",
+        description:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+    },
+];
+eventList.concat(eventList);
+const props = { eventList };
 export default function EventAdmin() {
-    console.log(props)
-    return (
-        <div className="mx-5 d-flex flex-column">
-            <h1 className="">
-                Events
-            </h1>
+    // render() {
+        console.log(props);
+        return (
+            <div className="mx-5 d-flex flex-column">
+                <h1 className="">
+                    Events
+                </h1>
 
-            <div className="w-100 d-flex flex-row my-4">
-                <div className="w-75">
-                    <input
-                        className="rounded"
-                        id=""
-                        name=""
-                        type="search"
-                    />
+                <div className="w-100 d-flex flex-row my-4">
+                    <div className="w-75">
+                        <input
+                            className="rounded"
+                            id=""
+                            name=""
+                            type="search"
+                        />
+                    </div>
+
+                    <div className="w-25 d-flex justify-content-center">
+                        <button
+                            className="p-3 border border-0 rounded"
+                            type="button"
+                        >
+                            Create Event
+                        </button>
+                    </div>
                 </div>
 
-                <div className="w-25 d-flex justify-content-center">
-                    <button
-                        className="p-3 border border-0 rounded"
-                        type="button"
+                <div className="d-flex flex-row justify-content-center">
+                    <Card
+                        className="m-3"
+                        style={{ width: "20rem" }}
                     >
-                        Create Event
-                    </button>
+                        <Card.Img
+                            src="https://via.placeholder.com/200x100"
+                            variant="top"
+                        />
+
+                        <Card.Body>
+                            <Card.Title className="d-flex justify-content-between">
+                                {props.name}
+
+                                <p className="fs-6 text-black-50">
+                                    {props.timestamp}
+                                </p>
+                            </Card.Title>
+
+                            <Card.Text>
+                                {props.description}
+                            </Card.Text>
+                        </Card.Body>
+
+                        <div className="d-flex flex-row justify-content-between text-xs px-3">
+                            <p className="fs-6">
+                                {props.clubName}
+                            </p>
+
+                            <p className="fs-6">
+                                Explore
+                            </p>
+                        </div>
+                    </Card>
                 </div>
             </div>
-
-            <div className="d-flex flex-row justify-content-center">
-                <Card
-                    className="m-3"
-                    style={{ width: '20rem' }}
-                >
-                    <Card.Img
-                        src="https://via.placeholder.com/200x100"
-                        variant="top"
-                    />
-
-                    <Card.Body>
-
-                        <Card.Title className="d-flex justify-content-between">
-                            Event Name
-
-                            <p className="fs-6 text-black-50">
-                                3 Days Ago
-                            </p>
-                        </Card.Title>
-
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-                        </Card.Text>
-                    </Card.Body>
-
-                    <div className="d-flex flex-row justify-content-between text-xs px-3">
-                        <p className="fs-6">
-                            Club Name
-                        </p>
-
-                        <p className="fs-6">
-                            Explore
-                        </p>
-                    </div>
-                </Card>
-
-                <Card
-                    className="m-3"
-                    style={{ width: '20rem' }}
-                >
-                    <Card.Img
-                        src="https://via.placeholder.com/200x100"
-                        variant="top"
-                    />
-
-                    <Card.Body>
-
-                        <Card.Title className="d-flex justify-content-between">
-                            Event Name
-
-                            <p className="fs-6 text-black-50">
-                                3 Days Ago
-                            </p>
-                        </Card.Title>
-
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-                        </Card.Text>
-                    </Card.Body>
-
-                    <div className="d-flex flex-row justify-content-between text-xs px-3">
-                        <p className="fs-6">
-                            Club Name
-                        </p>
-
-                        <p className="fs-6">
-                            Explore
-                        </p>
-                    </div>
-                </Card>
-
-                <Card
-                    className="m-3"
-                    style={{ width: '20rem' }}
-                >
-                    <Card.Img
-                        src="https://via.placeholder.com/200x100"
-                        variant="top"
-                    />
-
-                    <Card.Body>
-
-                        <Card.Title className="d-flex justify-content-between">
-                            Event Name
-
-                            <p className="fs-6 text-black-50">
-                                3 Days Ago
-                            </p>
-                        </Card.Title>
-
-                        <Card.Text>
-                            Some quick example text to build on the card title and make up the bulk of the card&apos;s content.
-                        </Card.Text>
-                    </Card.Body>
-
-                    <div className="d-flex flex-row justify-content-between text-xs px-3">
-                        <p className="fs-6">
-                            Club Name
-                        </p>
-
-                        <p className="fs-6">
-                            Explore
-                        </p>
-                    </div>
-                </Card>
-            </div>
-        </div>
-    );
+        );
+    // }
 }
+    
+
+EventAdmin.propTypes = {
+    clubName: PropTypes.string,
+    description: PropTypes.string,
+    name: PropTypes.string,
+    timestamp: PropTypes.string,
+};
