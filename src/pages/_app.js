1cde00bf9f4f196435d99d767850c671c3333735
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-11-07 12:24:25 +0530

    styling changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,17 @@
 /* eslint-disable */
-import './styles.css'
+// import './styles.css'
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {Styles} from "styles/styles";
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import {Styles} from "styles/styles";
 import BasicLayout from "../components/Layout";
 // import {SSRProvider} from 'react-bootstrap';
+import "styles/App.scss"
 
 export default function MyApp({Component, pageProps}) {
     const Layout = Component.Layout || BasicLayout;
     return (
         <Layout>
-            <Styles />
+            {/*<Styles />*/}
             <Component {...pageProps} />
         </Layout>
     )
