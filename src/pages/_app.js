98cc2773228ee3c58452d9a9e65e6a7c72163cd1
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-11-07 11:38:56 +0530

    minor

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,22 +1,18 @@
+/* eslint-disable */
 import './styles.css'
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Styles} from "styles/styles";
 import BasicLayout from "../components/Layout";
-import {StateMachineProvider} from "little-state-machine";
+// import {SSRProvider} from 'react-bootstrap';
 
-// This default export is required in a new `pages/_app.js` file.
-// eslint-disable-next-line react/prop-types
 export default function MyApp({Component, pageProps}) {
-    // eslint-disable-next-line react/prop-types
     const Layout = Component.Layout || BasicLayout;
     return (
-        <StateMachineProvider>
-            <Layout>
-                <Styles />
-
-                <Component {...pageProps} />
-            </Layout>
-        </StateMachineProvider>
-                )
+        <Layout>
+            <Styles />
+            <Component {...pageProps} />
+        </Layout>
+    )
 }
+
