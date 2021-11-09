64ff23811e1593f02a11983e481a7762f7093e94
Khwaish <khwaish.rupani@gmail.com>
2021-11-09 21:58:31 +0530

    minor

---
+++ src/components/FAQModal.jsx
@@ -0,0 +1,35 @@
+
+import React, {useState} from "react"
+import {Modal} from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+
+export default function FAQModal(props) {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.content}</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
