b0873c3a79b666092387721a0a367a268c39fa7c
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-11-11 10:10:18 +0530

    event page done

--- src/pages/event.js
+++ src/pages/event.js
@@ -3,7 +3,6 @@ import React from "react"
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import {Col, Row} from "react-bootstrap";
 
 
 class Event extends React.Component {
@@ -26,90 +25,126 @@ class Event extends React.Component {
 
     render() {
         return (
-            <div >
+            <>
                 <Container className="text-lg-center display-4 position-sticky my-3 bg-primary text-white">
                     {this.state.basicInformation.clubName}
                 </Container>
 
                 <Container className="pt-3 pb-3">
                     <div className="row">
-                        <Card className="col-9" bg="light">
-                            <Card.Body>
-                                <Card.Title className="row text-left pt-3">
-                                        <div className="col text-start">
-                                            <img className="rounded-3" src={this.state.basicInformation.poster}/>
+                        <Card
+                            bg="light"
+                            className="col-9"
+                        >
+                            <div className="row text-left p-3">
+                                <div className="col text-start">
+                                    <img
+                                        className="rounded-3"
+                                        src={this.state.basicInformation.poster}
+                                    />
+                                </div>
+
+                                <div className="col d-table">
+                                    <div className="d-table-cell align-middle">
+                                        <h3 className="text-center display-3 fw-bold">
+                                            {this.state.basicInformation.eventName}
+                                        </h3>
+
+                                        <div className="text-end text-primary pb-5 fs-5 mb-5">
+                                            ~By
+                                            {' '}
+
+                                            {this.state.basicInformation.clubName}
                                         </div>
-                                        <div className="col">
-                                            <div className="text-center display-3 fw-bold">
-                                                <br/>
-                                                <br/>
-                                                {this.state.basicInformation.eventName}
-                                            </div>
-                                            <div className="text-end">
-                                                ~By {this.state.basicInformation.clubName}
-                                            </div>
-                                            <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/> <br/>
-                                            <div className="row text-end">
-                                                <Button className="col-7 rounded-4 w-20 fs-4">
+
+
+
+                                        <div className="d-flex justify-content-around text-end pt-5">
+
+                                            <Button
+                                                className="rounded-4 w-20 fs-4 px-5"
+                                                variant="outline-primary"
+                                            >
+                                                FAQ
+                                            </Button>
+
+                                            <Button className="rounded-4 w-20 fs-4 px-3">
                                                 Participate
-                                                </Button>
-                                                <Button className="offset-1 col-4 rounded-4 w-20 fs-4">
-                                                    Faq
-                                                </Button>
-                                            </div>
+                                            </Button>
+
+
                                         </div>
-                                </Card.Title>
-                                <br />
-                                <Card.Text className="fs-5">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the standard dummy text ever since the 1500s,
-                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                    It has survived not only five centuries, but also the leap into electronic typesetting,
-                                    remaining essentially unchanged. It was popularised in the 1960s with the release
-                                    of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
-                                    publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                    Lorem Ipsum has been the standard dummy text ever since the 1500s,
-                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                                    It has survived not only five centuries, but also the leap into electronic typesetting,
-                                    remaining essentially unchanged. It was popularised in the 1960s with the release
-                                    of Letraset sheets
-                                </Card.Text>
-                            </Card.Body>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <p className="fs-5">
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release
+                                of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release
+                                of Letraset sheets Lorem Ipsum is simply dummy text of the printing and typesetting
+                                industry.
+                                Lorem Ipsum has been the standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen
+                                book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release
+                                of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                                publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                                Lorem Ipsum has been the standard dummy text ever since the 1500s,
+                                when an unknown printer took a galley of type and scrambled it to make a type specimen
+                                book.
+                                It has survived not only five centuries, but also the leap into electronic typesetting,
+                                remaining essentially unchanged. It was popularised in the 1960s with the release
+                                of Letraset sheets
+                            </p>
                         </Card>
 
-                        <div className="position-end col-3 fs-5">
-                            <Card>
+                        <div className=" col-3 fs-5 nd-0 ">
+                            <Card
+                                className="position-sticky end-0"
+                                style={{top:"5rem"}}
+                            >
                                 <Card.Body>
                                     <Card.Header className="text-center">
                                         Event Details
                                     </Card.Header>
                                     Detailed Timeline
 
-                                    <Card.Text>
-                                        <ul className="list-unsettled">
-                                            <li>
-                                                Registrations & Team Formation Starts on: 25th June 2021
-                                            </li>
+                                    <ul className="list-unsettled">
+                                        <li>
+                                            Registrations & Team Formation Starts on: 25th June 2021
+                                        </li>
 
-                                            <li>
-                                                Registrations & Team Formation Ends by: 26th July 2021
-                                            </li>
+                                        <li>
+                                            Registrations & Team Formation Ends by: 26th July 2021
+                                        </li>
 
-                                            <li>
-                                                Orientation Webinar: 31st July - Saturday - 11:30 AM to 1 PM
-                                                Last Date for Concept Note Submissions: 2nd August 2021
-                                            </li>
-                                        </ul>
-                                    </Card.Text>
+                                        <li>
+                                            Orientation Webinar: 31st July - Saturday - 11:30 AM to 1 PM
+                                            Last Date for Concept Note Submissions: 2nd August 2021
+                                        </li>
+                                    </ul>
                                 </Card.Body>
                             </Card>
+
                             <br />
                         </div>
                     </div>
                 </Container>
-            </div>
+            </>
         )
     }
-} 
+}
 export default Event
