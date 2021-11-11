6baeeb2e7ba6908982abd2c4a16c6756691b83ac
heemankv <vheemank@gmail.com>
2021-11-11 12:00:28 +0530

    carousel updates

--- src/pages/pathfinders/submissionPage.js
+++ src/pages/pathfinders/submissionPage.js
@@ -106,22 +106,18 @@ export default class submissionPage extends Component {
 
                             <br />
 
-                            <br />
-
 
                             <div className="well bg-white">
 
-                                <br />
-
-                                <Carousel>
+                                <Carousel className="">
                                     <Carousel.Item>
                                         <img
-                                            className="d-block w-100"
-                                            src="holder.js/800x400?text=First slide&bg=373940"
+                                            className=" w-100"
+                                            src="https://via.placeholder.com/350x150"
                                             alt="First slide"
                                         />
 
-                                        <Carousel.Caption>
+                                        {/* <Carousel.Caption>
                                             <h3>
                                                 First slide label
                                             </h3>
@@ -129,17 +125,17 @@ export default class submissionPage extends Component {
                                             <p>
                                                 Nulla vitae elit libero, a pharetra augue mollis interdum.
                                             </p>
-                                        </Carousel.Caption>
+                                        </Carousel.Caption> */}
                                     </Carousel.Item>
 
                                     <Carousel.Item>
                                         <img
-                                            className="d-block w-100"
-                                            src="holder.js/800x400?text=Second slide&bg=282c34"
+                                            className=" w-100"
+                                            src="https://via.placeholder.com/350x150"
                                             alt="Second slide"
                                         />
 
-                                        <Carousel.Caption>
+                                        {/* <Carousel.Caption>
                                             <h3>
                                                 Second slide label
                                             </h3>
@@ -147,17 +143,17 @@ export default class submissionPage extends Component {
                                             <p>
                                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                                             </p>
-                                        </Carousel.Caption>
+                                        </Carousel.Caption> */}
                                     </Carousel.Item>
 
                                     <Carousel.Item>
                                         <img
-                                            className="d-block w-100"
-                                            src="holder.js/800x400?text=Third slide&bg=20232a"
+                                            className="w-100"
+                                            src="https://via.placeholder.com/350x150"
                                             alt="Third slide"
                                         />
 
-                                        <Carousel.Caption>
+                                        {/* <Carousel.Caption>
                                             <h3>
                                                 Third slide label
                                             </h3>
@@ -165,13 +161,18 @@ export default class submissionPage extends Component {
                                             <p>
                                                 Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                                             </p>
-                                        </Carousel.Caption>
+                                        </Carousel.Caption> */}
                                     </Carousel.Item>
+
+                                    
                                 </Carousel>
                                 
                                 
                                 
                             </div>
+
+                            <br />
+
                         </div>
                     </div>
                 </Card>
