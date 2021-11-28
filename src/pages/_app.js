737cfc5498d81dae0045872309bda6b024b062bf
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-11-28 19:19:11 +0530

    layer0 RUM tracking

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,7 +4,14 @@ import BasicLayout from "../components/Layout";
 // import {SSRProvider} from 'react-bootstrap';
 import "styles/App.scss"
 
+import {Metrics} from '@layer0/rum'
+
+
 export default function MyApp({Component, pageProps}) {
+    new Metrics({
+        token: '0d764cb7-fbb3-4c5b-8a77-0483c87215c2'
+    }).collect()
+
     const Layout = Component.Layout || BasicLayout;
     return (
         <Layout>
