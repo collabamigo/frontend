a3a680a81503a52cb663759291b999bece3faa1b
heemankv <vheemank@gmail.com>
2021-12-17 11:47:29 +0530

    new updates 6

--- src/common/ScrollToTop/index.tsx
+++ src/common/ScrollToTop/index.tsx
@@ -34,7 +34,7 @@ const ScrollToTop = () => {
 
   return (
     <ScrollUpContainer onClick={scrollUp} show={showScroll}>
-      <SvgIcon src="scroll-top.svg" width="20px" height="20px" />
+      <SvgIcon src="scroll-top.svg" width="20px" height="20px" className="" />
     </ScrollUpContainer>
   );
 };
