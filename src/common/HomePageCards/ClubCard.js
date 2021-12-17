fa67a82eec979e52e70eeba0217600484edac9ba
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-17 11:13:51 +0530

    club picture in homepage

--- src/common/HomePageCards/ClubCard.js
+++ src/common/HomePageCards/ClubCard.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Card from 'react-bootstrap/Card'
 import PropTypes from "prop-types";
 import styles from "./ClubCard.module.css";
+import {getDownloadURL, getStorage, ref} from "firebase/storage";
 
 
-export default function ClubCard(props) {
+export default function ClubCard({value}) {
+    const [picture, setPicture] = useState("");
+    useEffect(() => {
+        if (!picture) {
+            const storage = getStorage();
+            getDownloadURL(ref(storage, 'data/' + value.username + '/uneditable/logo.png'))
+                .then(url => setPicture(url))
+        }
+    })
+
+    console.log(value)
     return (
         <div className={styles.cardCenter}>
             <Card>
                 <Card.Img
-                    src={props.value.picture}
+                    src={picture}
                     variant="top"
                 />
 
                 <Card.Body>
                     <Card.Title className={styles.title + " mb-2"}>
-                        {props.value.name}
+                        {value.name}
                     </Card.Title>
                 </Card.Body>
             </Card>
@@ -24,14 +35,5 @@ export default function ClubCard(props) {
 }
 
 ClubCard.propTypes = {
-    // eslint-disable-next-line react/forbid-prop-types
-    value: PropTypes.object,
-
-}
-
-ClubCard.defaultProps = {
-    value:{
-        src: "https://via.placeholder.com/450x450",
-        title: "Event title",
-    }
+    value: PropTypes.objectOf(PropTypes.string).isRequired
 }
