5c13571df3053cd3290de5bcceda177e6573a192
heemankv <vheemank@gmail.com>
2021-12-19 15:22:03 +0530

    responsive footer

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,38 +1,170 @@
-import React from "react";
-import {footer, linkFooter} from './Footer.module.css';
+import React from "react"
+import styles from "./Footer.module.css"
+function Footer() {
+  return (
+      <footer className={styles.site_footer}>
+          <div className="container">
+              <div className="row">
+                  <div className="col-sm-12 col-md-6">
+                      <h6>
+                          About
+                      </h6>
 
+                      <p className="text-justify">
+                          Scanfcode.com
+                          {' '}
 
-export default function Footer() {
-        return (
-            <footer className={'d-flex flex-row ' + footer}>
-                <div className={'col-md-7' + ' ' + linkFooter}>
-                    <b className="float-right pe-5">
-                        {/*eslint-disable-next-line*/}
-                        <a
-                            className={linkFooter}
-                            href="https://github.com/watson-hex/frontend-collabamigo/"
-                            rel="noopener"
-                            target="_blank"
-                            type="external"
-                        >
-                            Watson-Hex &#169; 2021
-                        </a>
-                    </b>
-                </div>
+                          <i>
+                              CODE WANTS TO BE SIMPLE
+                              {' '}
+                          </i>
 
-                <div className="col-md-5 float-right ">
-                    <a
-                        className={'float-right' + ' ' + linkFooter}
-                        href="https://forms.gle/MScukDTAhm2N3ixA8"
-                        rel="noreferrer"
-                        target="_blank"
-                    >
-                        Review Us
-                    </a>
-                </div>
+                          {' '}
+                          is an initiative  to help the upcoming programmers with the code. Scanfcode focuses on providing the most efficient code or snippets as the code wants to be simple. We will help programmers build up concepts in different programming languages that include C, C++, Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.
+                      </p>
+                  </div>
 
-            </footer>
-        )
-    }
+                  <div className="col-xs-6 col-md-3">
+                      <h6>
+                          Categories
+                      </h6>
 
+                      <ul className={styles.footer_links}>
+                          <li>
+                              <a href="http://scanfcode.com/category/c-language/">
+                                  C
+                              </a>
+                          </li>
 
+                          <li>
+                              <a href="http://scanfcode.com/category/front-end-development/">
+                                  UI Design
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/category/back-end-development/">
+                                  PHP
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/category/java-programming-language/">
+                                  Java
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/category/android/">
+                                  Android
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/category/templates/">
+                                  Templates
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+
+                  <div className="col-xs-6 col-md-3">
+                      <h6>
+                          Quick Links
+                      </h6>
+
+                      <ul className={styles.footer_links}>
+                          <li>
+                              <a href="http://scanfcode.com/about/">
+                                  About Us
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/contact/">
+                                  Contact Us
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/contribute-at-scanfcode/">
+                                  Contribute
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/privacy-policy/">
+                                  Privacy Policy
+                              </a>
+                          </li>
+
+                          <li>
+                              <a href="http://scanfcode.com/sitemap/">
+                                  Sitemap
+                              </a>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+
+              <hr />
+          </div>
+
+          <div className="container">
+              <div className="row">
+                  <div className="col-md-8 col-sm-6 col-xs-12">
+                      <p className={styles.copyright_text}>
+                          Copyright &copy; 2017 All Rights Reserved by 
+                          <a href="#">
+                              Scanfcode
+                          </a>
+                          .
+                      </p>
+                  </div>
+
+                  <div className="col-md-4 col-sm-6 col-xs-12">
+                      <ul className={styles.social_icons}>
+                          <li>
+                              <a
+                                  className="facebook"
+                                  href="#"
+                              >
+                                  <i className="fa fa-facebook" />
+                              </a>
+                          </li>
+
+                          <li>
+                              <a
+                                  className="twitter"
+                                  href="#"
+                              >
+                                  <i className="fa fa-twitter" />
+                              </a>
+                          </li>
+
+                          <li>
+                              <a
+                                  className="dribbble"
+                                  href="#"
+                              >
+                                  <i className="fa fa-dribbble" />
+                              </a>
+                          </li>
+
+                          <li>
+                              <a
+                                  className="linkedin"
+                                  href="#"
+                              >
+                                  <i className="fa fa-linkedin" />
+                              </a>
+                          </li>   
+                      </ul>
+                  </div>
+              </div>
+          </div>
+      </footer>
+  )
+}
+
+export default Footer
