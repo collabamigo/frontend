d518c2f64b8eaa1f46353637e500f56fe5fab964
Shikhar Sharma <40908524+Shikharhacks007@users.noreply.github.com>
2021-12-20 02:03:43 +0530

    temp update

--- src/pages/manage/[clubName]/index.jsx
+++ src/pages/manage/[clubName]/index.jsx
@@ -334,9 +334,9 @@ class ClubAdminPage extends Component {
         else{
             return(
                 <svg
-                    height="24"
+                    height="44"
                     viewBox="0 0 24 24"
-                    width="24"
+                    width="44"
                     xmlns="http://www.w3.org/2000/svg"
                 >
                     <path d="M19.5 12c-2.483 0-4.5 2.015-4.5 4.5s2.017 4.5 4.5 4.5 4.5-2.015 4.5-4.5-2.017-4.5-4.5-4.5zm2.5 5h-2v2h-1v-2h-2v-1h2v-2h1v2h2v1zm-18 0l4-5.96 2.48 1.96 2.52-4 1.853 2.964c-1.271 1.303-1.977 3.089-1.827 5.036h-9.026zm10.82 4h-14.82v-18h22v7.501c-.623-.261-1.297-.422-2-.476v-5.025h-18v14h11.502c.312.749.765 1.424 1.318 2zm-9.32-11c-.828 0-1.5-.671-1.5-1.5 0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5c0 .829-.672 1.5-1.5 1.5z" />
@@ -353,7 +353,14 @@ class ClubAdminPage extends Component {
             (new Date().getTime()));
         if(image == null)
             return;
-        uploadBytes(storageRef, image).then((args) => {console.log("test again1",args);})
+        uploadBytes(storageRef, image).then((args) => {console.log("test again1");
+            const arr = [args["metadata"]["fullPath"]]
+            const payload = {
+
+                picture: JSON.stringify(arr)
+            }
+            axios.patch("/club/club/" + this.props.router.query.clubName + "/", payload).then(()=>console.log("done"))
+        })
              .catch(() => {console.log("error bro")});
     }
 
@@ -634,14 +641,14 @@ class ClubAdminPage extends Component {
                                     
                                     <input
                                         className="d-none"
-                                        onChange={(e)=>this.handleUpload2(e.target.files[1])}
+                                        onChange={(e)=>this.handleUpload2(e.target.files[0])}
                                         ref={this.image2Ref}
                                         type="file"
                                     />
                                     
                                     <input
                                         className="d-none"
-                                        onChange={(e)=>this.handleUpload3(e.target.files[2])}
+                                        onChange={(e)=>this.handleUpload3(e.target.files[0])}
                                         ref={this.image3Ref}
                                         type="file"
                                     />
