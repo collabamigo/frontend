70ac356d82a4c83055813039d9802863ad55392d
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2021-12-20 11:56:06 +0530

    cleanup

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,5 +1,6 @@
 import React from "react"
-import styles from "styles/404.module.css";
+import styles from "./404.module.css";
+
 function NotFound() {
   return (
       <div className={styles.boddy}>
--- src/pages/formbuilder.jsx
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-import CreateEventModal from "../components/CreateEventModal";
-
-export default function FormBuilderPage() {
-    return (
-        <CreateEventModal />)
-}
