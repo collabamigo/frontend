fe8c0ff88e473d7a4a079803dac5296fca46b601
sc0rp10n-lappy-wslubuntu <pragyan52yadav@gmail.com>
2022-01-15 22:20:19 +0530

    fix 2

--- routes.js
+++ routes.js
@@ -14,18 +14,13 @@ router.prerender([
   // Default route
   {
     path: '*',
-    handler: (req, res) => {
-      res.status(404).send('Not found')
-    }
   },
   {
     path: '/',
-    handler: (req, res) => {
-      res.redirect('/home')
-    }
   },
   {
     path: '/event/:id',
+  }
 ])
 
 // Serve service worker
@@ -34,32 +29,32 @@ router.get('/service-worker.js', ({ serviceWorker }) => {
 })
 
 // SSR Cache Handler
-router.match('/', SSR_CACHE_HANDLER)
-router.match('/show/:path*', SSR_CACHE_HANDLER)
+// router.match('/', SSR_CACHE_HANDLER)
+// router.match('/show/:path*', SSR_CACHE_HANDLER)
 
 // Cache fonts
-router.match('/fonts/:file', ({ cache, serveStatic }) => {
-  cache(assetCache)
-  serveStatic('public/fonts/:file')
-})
-router.match('/assets/:file', ({ cache, serveStatic }) => {
-  cache(assetCache)
-  serveStatic('public/assets/:file')
-})
-router.match('/image/:path*', ({ cache, setResponseHeader, proxy }) => {
-  cache(assetCache)
-  setResponseHeader('cache-control', 'public, max-age=86400')
-  proxy('image', { path: ':path*' })
-})
-router.match('/_next/image/:path*', ({ cache, setResponseHeader, removeUpstreamResponseHeader }) => {
-  removeUpstreamResponseHeader('set-cookie')
-  setResponseHeader('cache-control', 'public, max-age=86400')
-  cache(assetCache)
-})
+// router.match('/fonts/:file', ({ cache, serveStatic }) => {
+//   cache(assetCache)
+//   serveStatic('public/fonts/:file')
+// })
+// router.match('/assets/:file', ({ cache, serveStatic }) => {
+//   cache(assetCache)
+//   serveStatic('public/assets/:file')
+// })
+// router.match('/image/:path*', ({ cache, setResponseHeader, proxy }) => {
+//   cache(assetCache)
+//   setResponseHeader('cache-control', 'public, max-age=86400')
+//   proxy('image', { path: ':path*' })
+// })
+// router.match('/_next/image/:path*', ({ cache, setResponseHeader, removeUpstreamResponseHeader }) => {
+//   removeUpstreamResponseHeader('set-cookie')
+//   setResponseHeader('cache-control', 'public, max-age=86400')
+//   cache(assetCache)
+// })
 
 // Next Cache Handler
-router.match('/_next/data/:build/index.json', NEXT_CACHE_HANDLER)
-router.match('/_next/data/:build/show/:name.json', NEXT_CACHE_HANDLER)
+// router.match('/_next/data/:build/index.json', NEXT_CACHE_HANDLER)
+// router.match('/_next/data/:build/show/:name.json', NEXT_CACHE_HANDLER)
 
 // Default Next.js Routes
 router.use(nextRoutes)
