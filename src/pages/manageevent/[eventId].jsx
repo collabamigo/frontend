182d8afd02d14ed202f15483e310cdbeda5b4983
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-02-03 17:07:58 +0530

    minor

--- src/pages/manageevent/[eventId].jsx
+++ src/pages/manageevent/[eventId].jsx
@@ -494,6 +494,7 @@ function Event() {
                             tarr = JSON.parse(response.elements[index].value.replace("'", "\""));
                     }
                     catch (e) {
+                        console.log("error in graphData", e);
                         tarr = [];
                     }
                     tarr.forEach((t) => {
@@ -508,7 +509,6 @@ function Event() {
                 })
                 chartData.datasets[0].data = Object.values(graphData);
                 chartData.labels = Object.keys(graphData);
-                // console.log("chartData", chartData);
                 return (
                     <>
                         <h1 key={option.name}>
@@ -525,6 +525,7 @@ function Event() {
                     </>
                 )
             } else {
+
                 return (
                     <>
                         <h1 key={option.name}>
