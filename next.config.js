1a946bafd5c8b6c657925e5594c65a637d7cd68c
sc0rp10n-lappy-wslubuntu <pragyan52yadav@gmail.com>
2022-02-09 13:28:25 +0530

    sentry added and tested

--- next.config.js
+++ next.config.js
@@ -8,7 +8,15 @@ const _preLayer0Export = {
     },
 };
 
-module.exports = (phase, config) =>
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = (phase, config) => {
+  // Your existing module.exports
   withLayer0(
     withServiceWorker({
       // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
@@ -16,5 +24,21 @@ module.exports = (phase, config) =>
       layer0SourceMaps: true,
 
       ..._preLayer0Export
-    })
-  )
+    }))
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
