028685e92239d0a0bac04b9c4dfe91327a0bca11
Aditya Pratap Singh <adityapratapsingh51@gmail.com>
2022-02-10 16:01:22 +0530

    bugfix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-// This file was automatically added by layer0 init.
-// You should commit this file to source control.
+
 const {withLayer0, withServiceWorker} = require('@layer0/next/config')
 
 const _initialExport = {
@@ -15,31 +14,19 @@ const _initialExport = {
 
 const {withSentryConfig} = require('@sentry/nextjs');
 
-const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+// module.exports = _sentryExport
+// eslint-disable-next-line no-unused-vars
+const _layer0Export =  withLayer0(
+        withServiceWorker(_initialExport),
+    )
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+console.log('_layer0Export', typeof _layer0Export, (_layer0Export));
 
 // Ensures that sentry isn't initialized if auth token isn't set
-let _sentryExport = _initialExport;
-if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' || process.env.SENTRY_AUTH_TOKEN)
-    _sentryExport = withSentryConfig(_initialExport, sentryWebpackPluginOptions);
+let _sentryExport = _layer0Export;
+if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'production' || process.env.NEXT_PUBLIC_SENTRY_DSN)
+    _sentryExport = withSentryConfig(_layer0Export);
 
-// eslint-disable-next-line no-unused-vars
-module.exports = (phase, config) =>
-    withLayer0(
-        withServiceWorker({
-            // Output sourcemaps so that stack traces have original source filenames and line numbers when tailing
-            // the logs in the Layer0 developer console.
-            layer0SourceMaps: true,
+console.log('_sentryExport', typeof _sentryExport, (_sentryExport));
 
-            ..._sentryExport
-        })
-    )
+module.exports = _sentryExport;
